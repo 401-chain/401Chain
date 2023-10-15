97fb01dca7b76d6d3d79372eb950e4081cfa86a6
Madhur Gupta <madhurgupta107@gmail.com>
2023-10-15 20:27:05 +0530

    Complete landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,14 +4,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="mt-[4vh]">
-      <div className="pl-[4vw] animate-fade-right animate-once animate-duration-1200 animate-delay-300 animate-ease-in-out">
-        <Image
-          src="/logo_full.png"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
+    <main className="flex justify-center">
+      <div className="mt-[15vh] pl-[4vw] animate-fade animate-once animate-duration-1200 animate-delay-300 animate-ease-in-out">
+        <Image src="/landing.png" width={800} height={600} alt="401Chain" />
       </div>
     </main>
   );
