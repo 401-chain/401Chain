1a5efe65cc019edec19d26cf851c79aca8dc1247
Madhur Gupta <madhurgupta107@gmail.com>
2023-10-15 15:19:26 +0530

    style: add slide in animation to landing img

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,13 +5,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="mt-[4vh]">
+      <div className="pl-[4vw] animate-fade-right animate-once animate-duration-1200 animate-delay-300 animate-ease-in-out">
         <Image
-        className="ml-[4vw]"
           src="/logo_full.png"
           width={500}
           height={500}
           alt="Picture of the author"
         />
+      </div>
     </main>
   );
 }
