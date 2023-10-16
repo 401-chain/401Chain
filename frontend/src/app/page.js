a5476a4ea73ac992fee29e0366bcd8cd6afae55c
Madhur Gupta <madhurgupta107@gmail.com>
2023-10-16 10:02:29 +0530

    Add scroll indicator on landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,19 @@
 "use client";
 
 import Image from "next/image";
+import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
-      <div className="mt-[15vh] pl-[4vw] animate-fade animate-once animate-duration-1200 animate-delay-300 animate-ease-in-out">
-        <Image src="/landing.png" width={800} height={600} alt="401Chain" />
+    <main>
+      <div className="flex justify-center items-center h-[85vh]">
+        <div className="animate-fade animate-once animate-duration-1200 animate-delay-300 animate-ease-in-out">
+          <Image src="/landing.png" width={800} height={600} alt="401Chain" />
+        </div>
+        <div id="mouse-scroll-animation" />
       </div>
+
+      <About />
     </main>
   );
 }
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="w-fit text-3xl font-bold text-[#2E8B57] mx-6 my-4 pb-4 border-b-[1px] border-[#2E8B57]">
+        About
+      </h2>
+    </div>
+  );
+};
+
+export default About;
